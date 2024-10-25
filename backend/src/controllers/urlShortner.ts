dce23a0d76645caf73d89e55cf1b2687d616d11c
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-10-25 20:46:52 +0530

    created the shorturl

--- backend/src/controllers/urlShortner.ts
+++ backend/src/controllers/urlShortner.ts
@@ -1,29 +1,50 @@
-import  Express from "express";
+import  express  from "express";
 import { shortnerModel } from "../models/urlShort";
 
 
-export const  createUrl = async (req,res) =>{
+
+export const  createUrl = async (
+    req: express.Request,
+    res: express.Response,
+) =>{
+    try {
+        const {fullUrl} = req.body;
+        const existingUrl = await shortnerModel.findOne({fullUrl})
+   if (existingUrl ) {
+    return res.status(409).json({
+        message: "URL already exists in the database",
+        existingUrl: existingUrl
+    })
+   } else{
+    const  shorturl = await shortnerModel.create({fullUrl})
+    res.status(201).send(shorturl)
+   }
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+export const  getallUrl = async ( 
+    req: express.Request,
+    res: express.Response,) =>{
     try {
         
     } catch (error) {
         
     }
 }
-export const  allUrl = async (req,res) =>{
+export const  getUrl = async ( 
+    req: express.Request,
+    res: express.Response,) =>{
     try {
         
     } catch (error) {
         
     }
 }
-export const  getUrl = async (req,res) =>{
-    try {
-        
-    } catch (error) {
-        
-    }
-}
-export const  deleteUrl = async (req,res) =>{
+export const  deleteUrl = async ( 
+    req: express.Request,
+    res: express.Response,) =>{
     try {
         
     } catch (error) {
--- backend/src/routes/Shortener.ts
+++ backend/src/routes/Shortener.ts
@@ -1,12 +1,12 @@
-import express  from "express";
-import { allUrl, createUrl, deleteUrl, getUrl } from "../controllers/urlShortner";
+import express ,  { Request, Response } from "express";
+import {getallUrl, createUrl, deleteUrl, getUrl } from "../controllers/urlShortner";
 
 
 
 const router = express.Router();
 
 router.post("/urlshort",createUrl)
-router.get("/urlshort",allUrl)
+router.get("/urlshort",getallUrl)
 router.get("/urlshort/:id",getUrl)
 router.post("/delete/:id",deleteUrl)
 
