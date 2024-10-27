18a8edb71a06c5c9f4217f0bea97f3e3de62a224
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-10-27 12:28:36 +0530

    setting up the client side

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+     <div> 
+      <Button>hiiiii</Button>
+     </div>
+    </>
+  )
+}
+
+export default App
