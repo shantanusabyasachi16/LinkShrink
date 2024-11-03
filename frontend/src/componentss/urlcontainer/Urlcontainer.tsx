3a782de4a52c351f14a01718afe65813f5437a8e
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-11-03 22:56:27 +0530

    created the input box

--- frontend/src/componentss/urlcontainer/Urlcontainer.tsx
+++ frontend/src/componentss/urlcontainer/Urlcontainer.tsx
@@ -1,18 +1,48 @@
 import * as React from 'react';
+import { Input } from '../../components/ui/input';
+import { Button } from '../../components/ui/button';
 
 
 interface IUrlcontainerProps {
 }
 
 const Urlcontainer: React.FunctionComponent<IUrlcontainerProps> = () => {
+ const[FullUrl,setFullUrl] = React.useState<string>("");
+ const handelSubmit = async (e: React .FormEvent<HTMLFormElement>)=>{
+e.preventDefault();
+try {
+  
+} catch (error) {
+  
+}
+ }
   return(
   <div className='container mx-auto p-2'>
   <div>
     <p className='text-2xl text-center'>The only URL Shortner
       <p className=' text-2xl text-center'>you'll ever need!</p>
-      
-    </p>
+      </p>
+<form onSubmit={handelSubmit}>
+  <div className='flex'>
+    <div className=' relative w-full'>
+      <Input
+    type=' text'
+    placeholder='Enter your link here...'
+    required
+    className='w-full text-gray-900 border rounded-lg'
+    value={FullUrl}
+    onChange={(e:React.ChangeEvent<HTMLInputElement>
+    
+    ) => setFullUrl(e.target.value)}
+    />
 
+
+    <Button type='submit' className=''>
+      Shorten URL
+    </Button>
+    </div>
+  </div>
+</form>
   </div>
 
   </div>
