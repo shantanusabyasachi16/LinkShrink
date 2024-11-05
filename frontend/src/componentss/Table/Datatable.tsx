b76c8b95a57e350bcd50911d834cbe524c630f50
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-11-06 00:01:09 +0530

    created the data table

---
+++ frontend/src/componentss/Table/Datatable.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+interface ITableProps {
+}
+
+const Table: React.FunctionComponent<ITableProps> = () => {
+  return  (
+    <div className='container'></div>
+  );
+};
+
+export default Table;
