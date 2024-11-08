23961378f8776e06d1a1415a0e8e102c8f86eee9
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-11-08 11:24:17 +0530

    debugged

--- frontend/src/componentss/urlcontainer/Urlcontainer.tsx
+++ frontend/src/componentss/urlcontainer/Urlcontainer.tsx
@@ -1,59 +1,65 @@
 import * as React from 'react';
-import { Input } from '../../components/ui/input';
-import { Button } from '../../components/ui/button';
+// import { Input } from '../../components/ui/input';
+// import { Button } from '../../components/ui/button';
 import axios from "axios"
-import { SERVER_URL } from '../../utils/endpoints';
+import { SERVER_URL } from '../../utils/Endpoints';
+// import { cn } from '../../lib/utils';
+import { Boxes } from "../../Boxes";
 
-interface IUrlcontainerProps {
-}
-
-const Urlcontainer: React.FunctionComponent<IUrlcontainerProps> = () => {
- const[FullUrl,setFullUrl] = React.useState<string>("");
- const handelSubmit = async (e: React .FormEvent<HTMLFormElement>)=>{
-e.preventDefault();
-try {
- await axios.post(`${SERVER_URL}/urlshort`,{
-  fullUrl:FullUrl,
- })
- setFullUrl("");
-} catch (error) {
-  console.log(error);
-  
-}
- }
-  return(
-  <div className='container mx-auto p-2'>
-  <div>
-    <p className='text-2xl text-center'>The only URL Shortner
-      <p className=' text-2xl text-center'>you'll ever need!</p>
-      </p>
-<form onSubmit={handelSubmit}>
-  <div className='flex'>
-    <div className=' relative w-full'>
-      <Input
-    type=' text'
-    placeholder='Enter your link here...'
-    required
-    className='w-full text-gray-900 border rounded-lg'
-    value={FullUrl}
-    onChange={(e:React.ChangeEvent<HTMLInputElement>
+const Urlcontainer = ()=>{
+  // interface IUrlcontainerProps {
+  //   }
     
-    ) => setFullUrl(e.target.value)}
-    />
+   
+     const[FullUrl,setFullUrl] = React.useState<string>("");
+     const handelSubmit = async (e: React .FormEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+    try {
+     await axios.post(`${SERVER_URL}/urlshort`,{
+      fullUrl:FullUrl,
+     })
+     setFullUrl("");
+    } catch (error) {
+      console.log(error);
+      
+    }
+     }
+  return(
+    <div className="h-96 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
+    <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+
+    <Boxes />
+    <h1 className='text-red-900'>heeloo</h1>
+   <div></div>
+  </div>
+  )
+}
+// 
+//   return(
+//     <BackgroundBoxesDemo>
+       
+//     className='w-full text-gray-900 border rounded-lg'
+//     value={FullUrl}
+//     onChange={(e:React.ChangeEvent<HTMLInputElement>
+    
+//     ) => setFullUrl(e.target.value)}
+//     />
 
 
-    <Button type='submit' className=''>
-      Shorten URL
-    </Button>
-    </div>
-  </div>
-</form>
-  </div>
+//     <Button type='submit' className=''>
+//       Shorten URL
+//     </Button>
+//     </div>
+//   </div>
+// </form>
+//   </div>
 
-  </div>
+//   </div>
+//     </BackgroundBoxesDemo>
+ 
 
    
-  ) ;
-};
+//   ) ;
+// };
 
 export default Urlcontainer;
